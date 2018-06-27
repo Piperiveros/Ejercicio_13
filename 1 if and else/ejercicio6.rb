7b0184bbe4351b760dcdaecaf4045aec85c852de
Piperiveros <felipe.riveros.f@gmail.com>
2018-06-27 19:14:50 -0400

    Agrega ejercicio if and else

---
+++ 1 if and else/ejercicio6.rb
@@ -0,0 +1,14 @@
+# Utiliza álgebra booleana para hacer un refactoring de este código
+# Para verificar la respuestas, puedes modificar los valores de a y b.
+
+a = 'falso'
+b = 'falso'
+
+if (a == 'verdadero' && b == 'falso') || (a == 'verdadero' && b == 'verdadero')
+    puts ':)'
+  elsif
+   b == 'verdadero' && a == 'falso'
+    puts ':|'
+  else
+    puts ':('
+  end
